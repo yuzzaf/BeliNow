39541fa7cdf9562d79f5401e02af465a23e308c2
Yleez <71832027+yuzzaf@users.noreply.github.com>
2026-02-26 13:56:25 +0700

    fix: routes

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/controller");
-const { isLoggedIn } = require("../middlewares/middleware");
 
-router.get("/", isLoggedIn, Controller.orderList);
+router.get("/", Controller.orderList);
 
 router.post("/create", Controller.createOrder);
 
-router.get("/:id", isLoggedIn, Controller.orderDetail);
+router.get("/:id", Controller.orderDetail);
 
 module.exports = router;
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/controller");
-const { isLoggedIn } = require("../middlewares/middleware");
 
-router.get("/:username", isLoggedIn, Controller.getProfile);
-router.get("/:username/edit", isLoggedIn, Controller.getProfileEdit);
-router.post("/:username/edit", isLoggedIn, Controller.postProfileEdit);
+router.get("/:username", Controller.getProfile);
+router.get("/:username/edit", Controller.getProfileEdit);
+router.post("/:username/edit", Controller.postProfileEdit);
 
 module.exports = router;
