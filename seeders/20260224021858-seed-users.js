2b764e3749518b1f176d478d02563dfc08bf226d
yuzzaf <muhammadyuzzaf@gmail.com>
2026-02-27 10:42:20 +0700

    fix: done

--- seeders/20260224021858-seed-users.js
+++ seeders/20260224021858-seed-users.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const fs = require("fs").promises;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
